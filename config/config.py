4be29ed5e821ba7a8d61e66b9afdfb0dd45acad5
Hardefa Rogonondo <hardefarogonondo@gmail.com>
2023-10-05 18:16:37 +0700

    Config updated

--- config/config.py
+++ config/config.py
@@ -1,7 +1,7 @@
 LOGGING_CONFIG = {
     "filename": 'log/app.log',
-    "level": 'logging.INFO'
+    "level": 20  # Value for logging.INFO
 }
 MKAD_CENTER = (55.75582620953044, 37.61730126651572)
 MKAD_RADIUS = 17.475
-YANDEX_API_KEY = "YOUR_API_KEY_HERE"
+YANDEX_API_KEY = "YOUR_API_KEY_HERE"  # Replace this with your own API Key
